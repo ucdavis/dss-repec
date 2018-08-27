033e7910ff8d47edd0db0f88c8220720f16d7b39
Sadaf Arshad <sarshad@ucdavis.edu>
2018-08-27 11:46:33 -0700

    Remove extra files.

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,7 +2,7 @@ class Paper < ApplicationRecord
   has_and_belongs_to_many :authors
   #has_one_attached :pdf
 
-  validates :title, :abstract, :presence => true 
+  validates :title, :abstract, :presence => true
 
   def self.title(title)
     if title
@@ -11,5 +11,4 @@ class Paper < ApplicationRecord
       all
     end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  #get 'authors/new'
-  #get 'browser/index'
-
   resources :papers
   resources :authors
 
-  #root 'browser#index'
-
   get '*path', to: 'browser#index', as: :pathway
-
-   # get '/browser/repec', to: 'browser#repec', as: "repec"
 end
