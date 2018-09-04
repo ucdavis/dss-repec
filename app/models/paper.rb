d289e9a6a632d817f0a47796fe0b8250d8de0ea1
Sadaf Arshad <sarshad@ucdavis.edu>
2018-09-04 11:10:44 -0700

    Verify one author is selected before creating a paper.

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,7 +2,7 @@ class Paper < ApplicationRecord
   has_and_belongs_to_many :authors
   #has_one_attached :pdf
 
-  validates :title, :abstract, :presence => true
+  validates :title, :abstract, :authors, :presence => true
 
   def self.search(search = nil)
     if search
