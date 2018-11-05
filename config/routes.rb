dd47c9e4a80c9f1f254f8011e87b768902c06bef
Christopher Thielen <cmthielen@ucdavis.edu>
2018-11-05 11:22:55 -0800

    Fix path for static archive files

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
     end
   end
 
+  # Static file rewrites. Must come before other 'repec/' paths
+  get 'repec/cda/cdaarch.rdf', to: static('cdaarch.rdf')
+  get 'repec/cda/cdaseri.rdf', to: static('cdaseri.rdf')
+
   get 'repec/cda/wpaper/:id.rdf', as: :paper_rdf , action: :show_rdf, controller: :browser
   get '/:path', to: 'browser#index', as: :pathway, constraints: { path: /repec[\/a-zA-Z]*/i }
 
