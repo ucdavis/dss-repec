d7bb66c143ef3f33bb8f81bd43b973b0742e32a7
Christopher Thielen <cmthielen@ucdavis.edu>
2018-11-30 12:21:07 -0800

    Link directly to file so RePEc service sees a HTTP 200, not HTTP 302

--- app/helpers/browser_helper.rb
+++ app/helpers/browser_helper.rb
@@ -8,4 +8,18 @@ module BrowserHelper
       return (str + file_size.to_s.rjust(13) + " " + link_to(body, url)).html_safe
     end
   end
+
+  # Loads the given 'url' and returns its 'Location' header, if any.
+  # This gets around a limitation of ActiveStorage where it can only provide
+  # links to 302s which then lead to the original file. This method of returning
+  # a 302 instead of a 200 is apparently incompatible with the RePEc service.
+  def resolve_redirect(url)
+    return nil unless url
+
+    uri = URI.parse(url)
+
+    response = Net::HTTP.get_response(uri)
+
+    response['Location']
+  end
 end
